ad8de214a45ed2f7831dc27924d040acf90eb2f3
Constantin Berhard <git.mail.enormator@xoxy.net>
2015-05-11 01:07:19 +0200

    startet working on gist API, removed useless code

--- functions.php
+++ functions.php
@@ -4,6 +4,24 @@ include_once("config.php");
 
 date_default_timezone_set($TIMEZONE);
 
+// TODO refine this to provide a gist generation API to the backends
+function getAgenda ($protocol) {
+	$i = 1; // the number of the TOP to search for
+	$items = array ();
+	foreach (explode("\n",$protocol) as $line) {
+		if (strpos($line,"$i") !== false && preg_match("/<h3>top|<h3>item/i", $line)) {
+			$line = strip_tags($line);
+			$line = preg_replace ("/(top|item)(?U).*$i/i", "", $line, 1);
+			$line = trim ($line, ": \t\n\r\0\x0B");
+			$i = $i + 1;
+			if (!empty($line)) {
+				array_push ($items, $line);
+			}
+		}
+	}
+	return $items;
+}
+
 function startsWith($haystack, $needle) {
 	return !strncmp($haystack, $needle, strlen($needle));
 }
